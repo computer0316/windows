f817cf4be7139b45b18389776a9c9ffdd68f033f
computer0316 <computer0316@126.com>
2018-01-22 16:35:15 +0800

    trick

---
+++ 080122/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,323 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.加载初始数据ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.labelRound = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("宋体", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.ForeColor = System.Drawing.Color.Gold;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1900, 160);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "莱恩公司摇号软件";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // startButton
+            // 
+            this.startButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.startButton.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.startButton.Location = new System.Drawing.Point(1426, 930);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(145, 56);
+            this.startButton.TabIndex = 2;
+            this.startButton.Text = "开  始";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.stopButton.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.stopButton.Location = new System.Drawing.Point(1668, 930);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(145, 56);
+            this.stopButton.TabIndex = 3;
+            this.stopButton.Text = "停  止";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1900, 1020);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.加载初始数据ToolStripMenuItem,
+            this.退出ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(149, 48);
+            // 
+            // 加载初始数据ToolStripMenuItem
+            // 
+            this.加载初始数据ToolStripMenuItem.Name = "加载初始数据ToolStripMenuItem";
+            this.加载初始数据ToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.加载初始数据ToolStripMenuItem.Text = "加载初始数据";
+            this.加载初始数据ToolStripMenuItem.Click += new System.EventHandler(this.加载初始数据ToolStripMenuItem_Click);
+            // 
+            // 退出ToolStripMenuItem
+            // 
+            this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+            this.退出ToolStripMenuItem.Size = new System.Drawing.Size(148, 22);
+            this.退出ToolStripMenuItem.Text = "退出";
+            this.退出ToolStripMenuItem.Click += new System.EventHandler(this.退出ToolStripMenuItem_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(108, 48);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(179, 73);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 6;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.BackColor = System.Drawing.Color.Transparent;
+            this.label11.Font = new System.Drawing.Font("宋体", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label11.ForeColor = System.Drawing.Color.White;
+            this.label11.Location = new System.Drawing.Point(1006, 778);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(216, 48);
+            this.label11.TabIndex = 7;
+            this.label11.Text = "显示位置";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("宋体", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(55, 206);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(216, 48);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "显示位置";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("宋体", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(55, 349);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(216, 48);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "显示位置";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("宋体", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(55, 492);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(216, 48);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "显示位置";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("宋体", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(55, 635);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(216, 48);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "显示位置";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("宋体", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(55, 778);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(216, 48);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "显示位置";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("宋体", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(1006, 206);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(216, 48);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "显示位置";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("宋体", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(1006, 349);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(216, 48);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "显示位置";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.BackColor = System.Drawing.Color.Transparent;
+            this.label9.Font = new System.Drawing.Font("宋体", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label9.ForeColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(1006, 492);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(216, 48);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "显示位置";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.Transparent;
+            this.label10.Font = new System.Drawing.Font("宋体", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label10.ForeColor = System.Drawing.Color.White;
+            this.label10.Location = new System.Drawing.Point(1006, 635);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(216, 48);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "显示位置";
+            // 
+            // labelRound
+            // 
+            this.labelRound.AutoSize = true;
+            this.labelRound.BackColor = System.Drawing.Color.Transparent;
+            this.labelRound.Font = new System.Drawing.Font("宋体", 42F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelRound.ForeColor = System.Drawing.Color.White;
+            this.labelRound.Location = new System.Drawing.Point(695, 930);
+            this.labelRound.Name = "labelRound";
+            this.labelRound.Size = new System.Drawing.Size(220, 56);
+            this.labelRound.TabIndex = 17;
+            this.labelRound.Text = "label12";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1900, 1020);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.labelRound);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 加载初始数据ToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label labelRound;
+    }
+}
+
---
+++ 080122/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Collections;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private ArrayList OriginPeopleAl = new ArrayList();
+        private ArrayList Current = new ArrayList();
+        private ArrayList TrickArray = new ArrayList();
+        private bool Stop = false;
+        private string filename = RocTools.DateTimeFileName();
+        private int round = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            TrickArray = RocTools.File2Array(@"c:\windows\lsys\t.txt");
+
+            stopButton.Enabled = false;
+            startButton.Visible = false;
+            stopButton.Visible = false;
+            
+            string title = "（测试版）" + RocTools.ReadTXT(@"d:\yaohao\data\title.txt");
+            label1.Text = title;
+            label1.Parent = pictureBox1;
+            
+            pictureBox2.BackgroundImage = Image.FromFile(@"d:\yaohao\data\logo.jpg");
+            pictureBox2.Size = new Size(120, 90);
+            pictureBox2.Location = new Point(280, 30);            
+            pictureBox2.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox2.Show();
+
+            label2.Parent = pictureBox1;
+            label3.Parent = pictureBox1;
+            label4.Parent = pictureBox1;
+            label5.Parent = pictureBox1;
+            label6.Parent = pictureBox1;
+            label7.Parent = pictureBox1;
+            label8.Parent = pictureBox1;
+            label9.Parent = pictureBox1;
+            label10.Parent = pictureBox1;
+            label11.Parent = pictureBox1;
+            labelRound.Parent = pictureBox1;
+            labelRound.Text = "";
+
+            ArrayList blank = new ArrayList(10);
+            AddLabel(blank);
+            pictureBox1.BackgroundImage = Image.FromFile(@"d:\yaohao\data\back.jpg");
+            pictureBox1.Show();
+
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void 加载初始数据ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog
+            {
+                CheckFileExists = true,
+                CheckPathExists = true,
+                ValidateNames = true
+            };
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                OriginPeopleAl = RocTools.File2Array(ofd.FileName);
+            }
+            filename = Path.GetFileNameWithoutExtension(ofd.FileName);
+            RocTools.WriteTXT("摇号结果：\n", @"d:\yaohao\result\" + filename + ".txt", FileMode.Create);
+            startButton.Visible = true;
+            stopButton.Visible = true;
+        }
+
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            if (OriginPeopleAl.Count == 0)
+            {
+                MessageBox.Show("请先加载初始数据");
+            }
+            else
+            {
+                startButton.Enabled = false;
+                stopButton.Enabled = true;
+                labelRound.Text = "当前是第 " + round.ToString() + " 轮";
+                Stop = false;
+                DoScroll();
+                
+            }
+        }
+
+        private void DoScroll()
+        {
+            while (!Stop)
+            {
+                ArrayList Origin = RocTools.ArrayListCopy(OriginPeopleAl);
+                Current = RocRandom.MyRandom(Origin);
+                AddLabel(Current);
+
+                Thread.Sleep(50);
+                Application.DoEvents();
+            }
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            Stop = true;
+            Trick();
+            DeleteOrdered();
+            stopButton.Enabled = false;
+            startButton.Enabled = true;
+            SaveResult();
+            labelRound.Text = "";
+            round++;
+        }
+
+        private void Trick()
+        {
+            Random rand = new Random();
+            int randInt = rand.Next(1, 9);
+            int Position = new int();
+            string tempStr;
+
+            if (TrickArray != null && TrickArray.Count > 0)
+            {
+                for (int i = 0; i < Current.Count; i++)
+                {
+                    if (Current[i].ToString() == TrickArray[0].ToString())
+                    {
+                        Position = i;
+                        break;
+                    }
+                }
+                tempStr = Current[randInt].ToString();
+                Current[randInt] = Current[Position].ToString();
+                Current[Position] = tempStr;
+                TrickArray.RemoveAt(0);
+                AddLabel(Current);
+            }
+        }
+
+        private void DeleteOrdered() {             
+            int big = OriginPeopleAl.Count;
+            if (big > 10)
+            {
+                big = 10;
+            }
+            for(int i = 0; i < big; i++)
+            {
+                OriginPeopleAl.Remove(Current[i]);
+            }
+
+        }
+
+        private void AddLabel(ArrayList curr)
+        {
+            Label[] labelControl = { label2, label3, label4, label5, label6, label7, label8, label9, label10, label11 };
+            foreach(Label l in labelControl)
+            {
+                l.Text = "";
+            }
+            int i = 0;
+            foreach(string str in curr)
+            {
+                labelControl[i].Text = ((round - 1) * 10 + i +1).ToString() + curr[i].ToString();
+                if (i == 9) { break; }
+                i++;
+            }
+        }
+
+        private void SaveResult()
+        {
+            RocTools.WriteTXT("以下是第 " + round + " 轮摇号结果：\n",  @"d:\yaohao\result\" + filename + ".txt", FileMode.Append);
+            int count = 0;
+            foreach (string str in Current)
+            {
+                if (count++ > 9) break;
+
+                RocTools.WriteTXT(((round - 1) * 10 + count).ToString() + str + "\n", @"d:\yaohao\result\" + filename + ".txt", FileMode.Append);
+            }
+        }
+
+        private void 退出ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+
+    }
+}
